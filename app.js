4031ecb0233ebf1ee660eb3ddeffdf0c2c235135
arifpro <arif.swfu@outlook.com>
2020-06-12 21:03:07 +0800

    js file

---
+++ app.js
@@ -0,0 +1,20 @@
+// set initial count
+let count = 0
+
+// select value and buttons
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(btn => btn.addEventListener('click', e => {
+    const styles = e.currentTarget.classList
+
+    if (styles.contains('increase')) {
+        count++
+    } else if (styles.contains('decrease')) {
+        count--
+    } else {
+        count = 0
+    }
+
+    value.textContent = count
+}))
